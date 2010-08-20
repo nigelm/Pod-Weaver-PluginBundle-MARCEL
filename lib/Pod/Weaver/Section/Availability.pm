0b550c86756056a2088e993ebb70b20f9fe8ea57
Nigel Metheringham <nigel.metheringham@dev.intechnology.co.uk>
2010-08-20 16:13:00 +0100

    snapshot of one approach - going to change the mechanisms dramatically from this

--- lib/Pod/Weaver/Section/Availability.pm
+++ lib/Pod/Weaver/Section/Availability.pm
@@ -10,32 +10,76 @@ with 'Pod::Weaver::Role::Section';
 use namespace::autoclean;
 use Moose::Autobox;
 
+# add a set of attributes to hold the repo information
+has dist_name => ( is => 'rw', type => 'Str' );
+has homepage_url  => ( is => 'rw', type => 'Str' );
+has repo_type => ( is => 'rw', type => 'Str', default => 'git' );
+has repo_url  => ( is => 'rw', type => 'Str' );
+has repo_web  => ( is => 'rw', type => 'Str', clearer => '_clear_repo_web', predicate => 'has_repo_web', );
+has is_github => ( is => 'rw', type => 'Bool', default => 0 );
+
 sub weave_section {
-    my ($self, $document, $input) = @_;
-    my $name = $input->{zilla}->name;
+    my ( $self, $document, $input ) = @_;
+
+    $self->_extract_dzil_metadata($input);
     $document->children->push(
         Pod::Elemental::Element::Nested->new(
-            {   command  => 'head1',
+            {
+                command  => 'head1',
                 content  => 'AVAILABILITY',
-                children => [
-                    Pod::Elemental::Element::Pod5::Ordinary->new(
-                        {   content => <<EOPOD,
-The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
-site near you, or see
-L<http://search.cpan.org/dist/$name/>.
-
-The development version lives at
-L<http://github.com/hanekomu/$name/>.
-Instead of sending patches, please fork this project using the standard git
-and github infrastructure.
-EOPOD
-                        }
-                    ),
-                ],
+                children => [ $self->_cpan_pod, $self->_development_pod, ],
             }
         ),
     );
+}
+
+# _extract_dzil_metadata uses the input hash passed across to
+# pull the Dist::Zilla metadata out and use it to populate the
+# attributes.  I would prefer this to be done as a BUILDARGS
+# mechanism (and hence also mark all the attributes r/o, but
+# this does not appear to be possible with the current framework)
+sub _extract_dzil_metadata {
+    my ( $self, $input ) = @_;
+
+    # if there is no zilla section we are sunk - bail out quietly
+    my $zilla = $input->{zilla} or return;
+
+    # set the distribution name
+    $self->dist_name( $zilla->name );
+
+    # die if there is no distmeta section - this means dzil not set right
+    my $meta = eval { $zilla->distmeta }
+      or die "No distmeta data present";
+
+    # pull repo out of distmeta resources.
+    my $repo = $meta->{resources}{repository};
+    if ($repo) {
+        $self->repo_type( $repo->{type} ) if ( $repo->{type} );
+        $self->_set_repo( $repo->{url} );
+    }
+}
+
+sub _set_repo {
+    my ( $self, $repourl ) = @_;
+
+    $self->is_github( ( $repourl =~ m|/github.com/| ) ? 1 : 0 );
+    $self->_clear_repo_web;
+    if ( $self->is_github ) {
+
+        # strip the access method off - we can then add it as needed
+        my $nomethod = $repourl;
+        $nomethod =~ s!^(http|git|git\@github\.com):/*!!i;
+        $self->repo_url( 'git://' . $nomethod );
+        $self->repo_web( 'http://' . $nomethod );
+    }else{
+        $self->repo_url( $repourl );
+    }
+}
+
+sub _cpan_pod {
+    my $self = shift;
+    
+    
 }
 1;
 
