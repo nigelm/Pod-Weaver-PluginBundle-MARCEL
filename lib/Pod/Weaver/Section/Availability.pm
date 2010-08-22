1cbbc589a4f69a2298bdfde8be3335e6b20f1dae
Nigel Metheringham <nigelm@cpan.org>
2010-08-22 15:09:32 +0100

    Untested code for new functionality

--- lib/Pod/Weaver/Section/Availability.pm
+++ lib/Pod/Weaver/Section/Availability.pm
@@ -11,76 +11,127 @@ use namespace::autoclean;
 use Moose::Autobox;
 
 # add a set of attributes to hold the repo information
-has dist_name => ( is => 'rw', type => 'Str' );
-has homepage_url  => ( is => 'rw', type => 'Str' );
-has repo_type => ( is => 'rw', type => 'Str', default => 'git' );
-has repo_url  => ( is => 'rw', type => 'Str' );
-has repo_web  => ( is => 'rw', type => 'Str', clearer => '_clear_repo_web', predicate => 'has_repo_web', );
-has is_github => ( is => 'rw', type => 'Bool', default => 0 );
+has zilla => ( is => 'rw', isa => 'Dist::Zilla', handles => [ 'name', 'distmeta' ] );
+has homepage_url => ( is => 'rw', isa => 'Str',  lazy => 1, builder => '_build_homepage_url' );
+has cpan_url     => ( is => 'rw', isa => 'Str',  lazy => 1, builder => '_build_cpan_url' );
+has repo_type    => ( is => 'rw', isa => 'Str',  lazy => 1, builder => '_build_repo_type' );
+has repo_url     => ( is => 'rw', isa => 'Str',  lazy => 1, builder => '_build_repo_url' );
+has repo_web     => ( is => 'rw', isa => 'Str',  lazy => 1, builder => '_build_repo_web' );
+has is_github    => ( is => 'rw', isa => 'Bool', lazy => 1, builder => '_build_is_github' );
 
 sub weave_section {
     my ( $self, $document, $input ) = @_;
 
-    $self->_extract_dzil_metadata($input);
+    $self->zilla( $input->{zilla} );
     $document->children->push(
         Pod::Elemental::Element::Nested->new(
             {
                 command  => 'head1',
                 content  => 'AVAILABILITY',
-                children => [ $self->_cpan_pod, $self->_development_pod, ],
+                children => [ $self->_homepage_pod, $self->_cpan_pod, $self->_development_pod, ],
             }
         ),
     );
 }
 
-# _extract_dzil_metadata uses the input hash passed across to
-# pull the Dist::Zilla metadata out and use it to populate the
-# attributes.  I would prefer this to be done as a BUILDARGS
-# mechanism (and hence also mark all the attributes r/o, but
-# this does not appear to be possible with the current framework)
-sub _extract_dzil_metadata {
-    my ( $self, $input ) = @_;
+sub _build_homepage_url {
+    my $self = shift;
 
-    # if there is no zilla section we are sunk - bail out quietly
-    my $zilla = $input->{zilla} or return;
-
-    # set the distribution name
-    $self->dist_name( $zilla->name );
-
-    # die if there is no distmeta section - this means dzil not set right
-    my $meta = eval { $zilla->distmeta }
-      or die "No distmeta data present";
-
-    # pull repo out of distmeta resources.
-    my $repo = $meta->{resources}{repository};
-    if ($repo) {
-        $self->repo_type( $repo->{type} ) if ( $repo->{type} );
-        $self->_set_repo( $repo->{url} );
-    }
+    return $self->distmeta->{resources}{homepage} || sprintf( 'http://search.cpan.org/dist/%s/', $self->name );
 }
 
-sub _set_repo {
-    my ( $self, $repourl ) = @_;
+sub _build_cpan_url {
+    my $self = shift;
 
-    $self->is_github( ( $repourl =~ m|/github.com/| ) ? 1 : 0 );
-    $self->_clear_repo_web;
+    return sprintf( 'http://search.cpan.org/dist/%s/', $self->name );
+}
+
+sub _build_repo_type {
+    my $self = shift;
+
+    # if we don't know we default to git...
+    return $self->distmeta->{resources}{repository}{type} || 'git';
+}
+
+sub _build_repo_url {
+    my $self = shift;
+
+    return ( $self->_build_repo_data )[0];
+}
+
+sub _build_repo_web {
+    my $self = shift;
+
+    return ( $self->_build_repo_data )[1];
+}
+
+sub _build_is_github {
+    my $self = shift;
+
+    # we do this by looking at the URL for githubbyness
+    my $repourl = $self->distmeta->{resources}{repository}{url} or die "No repository URL set in distmeta";
+    return ( ( $repourl =~ m|/github.com/| ) ? 1 : 0 );
+}
+
+sub _build_repo_data {
+    my $self = shift;
+
+    my $repourl = $self->distmeta->{resources}{repository}{url} or die "No repository URL set in distmeta";
+    my $repoweb;
     if ( $self->is_github ) {
 
         # strip the access method off - we can then add it as needed
         my $nomethod = $repourl;
         $nomethod =~ s!^(http|git|git\@github\.com):/*!!i;
-        $self->repo_url( 'git://' . $nomethod );
-        $self->repo_web( 'http://' . $nomethod );
-    }else{
-        $self->repo_url( $repourl );
+        $repourl = 'git://' . $nomethod;
+        $repoweb = 'http://' . $nomethod;
     }
+    return ( $repourl, $repoweb );
+}
+
+sub _homepage_pod {
+    my $self = shift;
+
+    # we suppress this if the CPAN URL is the homepage URL
+    return if ( $self->cpan_url eq $self->homepage_url );
+
+    # otherwise return some boilerplate
+    return Pod::Elemental::Element::Pod5::Ordinary->new(
+        { content => sprintf( 'The project homepage is L<%s>.', $self->homepage_url ) } );
 }
 
 sub _cpan_pod {
     my $self = shift;
-    
-    
+
+    my $text = sprintf(
+        "%s\n%s\n%s L<%s>.",
+        'The latest version of this module is available from the Comprehensive Perl',
+        'Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN',
+        'site near you, or see',
+        $self->cpan_url
+    );
+
+    return Pod::Elemental::Element::Pod5::Ordinary->new( { content => $text } );
 }
+
+sub _development_pod {
+    my $self = shift;
+
+    my $text;
+    if ( $self->is_github ) {
+        $text = sprintf( "The development version lives at L<%s>\n", $self->repo_web );
+        $text .= sprintf( "and may be cloned from L<%s>.\n", $self->repo_url );
+        $text .= "Instead of sending patches, please fork this project using the standard\n";
+        $text .= "git and github infrastructure.\n"
+
+    }
+    else {
+        $text = sprintf( "The development version lives in a %s repository at L<%s>\n", $self->repo_type, $self->repo_web );
+    }
+
+    return Pod::Elemental::Element::Pod5::Ordinary->new( { content => $text } );
+}
+
 1;
 
 =pod
